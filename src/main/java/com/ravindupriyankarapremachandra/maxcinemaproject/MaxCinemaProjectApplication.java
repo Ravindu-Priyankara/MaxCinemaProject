7d8f0825e00c16f2fede1b86c567c3ddc42010e2
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2022-12-30 20:21:57 +0530

    Initial commit

---
+++ src/main/java/com/ravindupriyankarapremachandra/maxcinemaproject/MaxCinemaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ravindupriyankarapremachandra.maxcinemaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxCinemaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxCinemaProjectApplication.class, args);
+	}
+
+}
