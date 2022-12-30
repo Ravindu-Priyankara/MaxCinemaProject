3e04e51bcbe55bff9769c41310509ce9df7a8859
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2022-12-31 01:03:44 +0530

    Today works

--- src/main/java/com/ravindupriyankarapremachandra/maxcinemaproject/MaxCinemaProjectApplication.java
+++ src/main/java/com/ravindupriyankarapremachandra/maxcinemaproject/MaxCinemaProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MaxCinemaProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaxCinemaProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MaxCinemaProjectApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/ravindupriyankarapremachandra/maxcinemaproject/controllers/seatController.java
@@ -0,0 +1,28 @@
+package com.ravindupriyankarapremachandra.maxcinemaproject.controllers;
+
+import com.ravindupriyankarapremachandra.maxcinemaproject.models.BookSeat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class seatController {
+
+    @GetMapping("/seats")
+    public String seats(Model model){
+        model.addAttribute("userSeat",new BookSeat());
+        model.addAttribute("filmName","Avatar 02"); // use this on database
+
+        return "seat";
+    }
+    @PostMapping("bookseat")
+    public String getSeat(@ModelAttribute BookSeat bookSeat, Model model){
+        if(bookSeat.isSeat01()){
+            System.out.println("yes");
+        }
+        model.addAttribute("userSeat",new BookSeat());
+        return "seat";
+    }
+}
